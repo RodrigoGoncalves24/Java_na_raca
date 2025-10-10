445ae7f5be00a77f13151c5d58d588f02082ade8
Rodrigo Goncalves <rodrigogoncalves2408@gmail.com>
2025-10-10 13:08:54 -0300

    Criação projeto

---
+++ src/array/meuArray.java
@@ -0,0 +1,14 @@
+package array;
+
+public class meuArray {
+
+    //ESTRUTA DE DEDOS DE UM ARRAY
+
+    //ADD ELEMENTO - EXPANDE QUANDO CEHIO
+
+    //REMOVE ELEMENTO COM ORGANIZAÇÃO
+
+    //GET PARA POSIÇÃO
+
+    //SIZE()
+}
