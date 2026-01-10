d30e27f51e04e59f09a724d611ac0a792742a23a
Rodrigo Goncalves <rodrigogoncalves2408@gmail.com>
2026-01-10 18:33:54 -0300

    Adicionado estrura map

--- src/map/meuMap.java
+++ src/map/meuMap.java
@@ -4,16 +4,16 @@ public class meuMap {
     private int[] keys;
     private String[] values;
     private int size;
-    private int tamanho = 10;
+    private int tamanho = 5;
 
     /// constructor
     public meuMap() {
         keys = new int[tamanho];
         values = new String[tamanho];
-        size = 0;
+        size = 1;
     }
 
-    /// put
+    /// put - change the value with that key
     public void put(int key, String value) {
         if (size == tamanho) doubleSize();
         int index = indexOfKey(key);
@@ -29,8 +29,15 @@ public class meuMap {
 
     ///  contains key
     public boolean containsKey(int key) {
-        for (int i = 0; i < size - 1; i++) {
+        for (int i = 0; i <= size - 1; i++) {
             if (keys[i] == key) return true;
+        }
+        return false;
+    }
+
+    public boolean containsValue(String value){
+        for (int i = 0; i < size - 1; i++){
+            if(values[i].equalsIgnoreCase(value) ) return true;
         }
         return false;
     }
@@ -66,6 +73,29 @@ public class meuMap {
         }
         values = tempValue;
         keys = tempKey;
+    }
+
+    /// remove
+    public void remove(int key){
+        int index = indexOfKey(key);
+        if(index != -1){
+            for(int i = index; i < size; i++){
+                values[i] = values[i+1];
+                keys[i] = keys[i+1];
+            }
+            size--;
+        }
+    }
+
+
+    public boolean isEmpty(){
+        return size == 0;
+    }
+
+    public void clear(){
+        values = new String[tamanho];
+        keys = new int[tamanho];
+        size = 0;
     }
 
 }
--- src/map/testeMap.java
+++ src/map/testeMap.java
@@ -10,27 +10,52 @@ public class testeMap {
         Random rand = new Random();
 
 
-        List<String> nomes = Arrays.asList("A", "B", "C", "D", "E", "F", "G");
+        List<String> nomes = Arrays.asList("Ana", "Bia", "Carlos", "Diogo", "Edu", "Fabiana", "Gustavo");
         System.out.println("************ TESTE MAP ************");
         System.out.println("Inserindo valor:");
         map.put(1, "Rodrigo");
-        System.out.println("Pegando valores: "+map.get(2));
-        map.put(2, "Leonardo");
         System.out.println("Pegando valores: "+map.get(1));
+        map.put(2, "Leonardo");
+        System.out.println("Pegando valores: "+map.get(2));
+        System.out.println("\nVerificando se a chave 2 existe no mapa: "+map.containsKey(2));
 
-        System.out.println("\nInserindo valor e dobrando de tamanho...");
-        for(int i = 0; i< 15; i++){
+        System.out.println("\nInserindo valores");
+        for(int i = 0; i<10; i++){
             map.put(i, nomes.get(rand.nextInt(0, 7)));
         }
 
+        System.out.println("Teste trocando elementos: Trocando elemento 3 -> "+map.get(3));
+        map.put(3, "Rodrigo");
+        System.out.println(map.get(3));
+
         System.out.println("Valores inseridos e map dobrado de tamanho!!");
-        for(int i = 0; i< map.size(); i++){
-            System.out.println(i+"° - "+map.get(i));
+        mostraMapa(map);
+
+        System.out.println("\nTeste remoção de elemento: ");
+        System.out.println("Removendo chave 2 -> "+map.get(2));
+        map.remove(2);
+        mostraMapa(map);
+
+        System.out.println("\nPesquisando se valor existe na lista");
+        System.out.println("Procurando a letra 'A'");
+        System.out.println(map.containsValue("Pedro"));
+        System.out.println(map.containsValue("Ana"));
+        System.out.println(map.containsValue("Carlos"));
+
+
+        System.out.println("\nEsta vazia? "+map.isEmpty());
+
+        System.out.println("\nLimpando estrutura: ");
+        map.clear();
+        System.out.println(map.size());
+    }
+
+
+    public static void mostraMapa(meuMap map){
+        for(int i = 0; i <= map.size(); i++){
+            if (map.get(i) != null){
+                System.out.println(i+"° - "+map.get(i));
+            }
         }
-
-        System.out.println("Teste trocando elementos");
-        map.put(10, "Rodrigo");
-        System.out.println(map.get(10));
-
     }
 }
